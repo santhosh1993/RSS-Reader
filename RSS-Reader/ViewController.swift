6806bd62e4b282cf1d6f4878853a92f0145bdc5c
Santhosh <santhosh_nampally@epam.com>
2019-05-29 11:59:06 +0530

    created new project

---
+++ RSS-Reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSS-Reader
+//
+//  Created by Santhosh Nampally on 29/05/19.
+//  Copyright © 2019 Santhosh Nampally. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
